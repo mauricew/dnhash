887fddc3a4dd71d440339dd09febdbb005c76032
Maurice Wahba <maurice.wahba@gmail.com>
2017-06-18 16:03:01 -0400

    Add "msdn" app and initial models

---
+++ msdn/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+class Language(models.Model):
+    code = models.CharField(max_length=4, unique=True)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class ProductGroup(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class ProductFamily(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    group = models.ForeignKey('ProductGroup')
+
+    def __str__(self):
+        return self.name
+
+class File(models.Model):
+    id = models.IntegerField(primary_key=True)
+    product_family = models.ForeignKey('ProductFamily')
+    file_name = models.CharField(max_length=1024)
+    sha1_hash = models.CharField(max_length=40)
+    description = models.CharField(max_length=1024)
+    notes = models.TextField(null=True)
+    posted_date = models.DateTimeField()
+    size = models.DecimalField(max_digits=12, decimal_places=3)
+    product_key_required = models.BooleanField()
+
+    def __str__(self):
+        return self.description
